d893dd3c1a6a3b1f2a7983bdbbad9d52874fb9e0
kyleerod <kyleerod@student.21-school.ru>
2022-12-16 22:43:38 +0700

    fc

---
+++ FilesCreator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilesCreator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s ="";
+             double a = 1000;
+             for (int i = 0; i < a; i++)
+             {
+                 double x = -5 + 10 / a * i;
+                 s += (x).ToString() +'\t';
+                 s += (Math.Sin(x)).ToString()  +"\t1\n";
+             } 
+
+          /*  for (int i = -5; i < 5; i++)
+            {
+                s += (i).ToString() + '\t'+(Math.Pow(i,3)).ToString()+'\n';
+            }*/
+
+                System.IO.File.WriteAllText("..\\...\\...\\file.txt", s);
+        }
+    }
+}
